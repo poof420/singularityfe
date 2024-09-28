1599ff6908a83b96e9b29a8ff6256ea178ae1c9b
poof_eth <sp1cydiscourse@gmail.com>
2024-09-28 15:12:27 -0500

    here

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
+// next.config.mjs
+
+import { fileURLToPath } from 'url';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    // Add this to ignore certain warnings
+    config.ignoreWarnings = [
+      { module: /node_modules\/node-fetch\/lib\/index\.js/ },
+      { file: /node_modules\/node-fetch\/lib\/index\.js/ },
+    ];
+
+    // Resolve __dirname in ESM
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        __dirname: fileURLToPath(new URL('.', import.meta.url)),
+      };
+    }
+
+    return config;
+  },
+};
 
 export default nextConfig;
